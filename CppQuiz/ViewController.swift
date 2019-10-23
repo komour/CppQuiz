da6b9dcd8c549dcf796010b7c2700e4a34e1c8ea
Komarov <komaroff404@gmail.com>
2019-10-23 23:55:58 +0300

    UIPickerView added

--- CppQuiz/ViewController.swift
+++ CppQuiz/ViewController.swift
@@ -8,16 +8,49 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        1
+    }
+    
 
+    var pickOptions = ["The program is guaranteed to output.", "Compilation error.", "Unspecified / Implementation defined.", "Undefined behavior."]
+    
+    var someText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupTextFields()
+        let pickerView = UIPickerView()
+        pickerView.delegate = self
+        textFieldPicker.inputView = pickerView
+        
     }
     
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
     
-    @IBOutlet weak var textField2: UITextField!
-    @IBOutlet weak var textField1: UITextField!
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickOptions.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return pickOptions[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        textFieldPicker.text = pickOptions[row]
+    }
+
+    @IBOutlet weak var viewScrollView: UIView!
+    @IBOutlet weak var textFieldAnswer: UITextField!
+    @IBOutlet weak var textFieldPicker: UITextField!
+    
+    func setupPickerView() {
+        
+    }
+    
     func setupTextFields() {
         let toolBar = UIToolbar(frame: CGRect(origin: .zero, size: .init(width: view.frame.size.width, height: 30)))
         let flexSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
@@ -26,8 +59,8 @@ class ViewController: UIViewController {
         toolBar.setItems([flexSpace, doneButton], animated: false)
         toolBar.sizeToFit()
         
-        textField1.inputAccessoryView = toolBar
-        textField2.inputAccessoryView = toolBar
+        textFieldAnswer.inputAccessoryView = toolBar
+        textFieldPicker.inputAccessoryView = toolBar
     }
     
     @objc func doneButtonAction() {
