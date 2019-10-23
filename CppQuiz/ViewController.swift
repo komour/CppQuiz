22b7dc230a370cb9e17cc2975aa7fd0248e0fcbc
Komarov <komaroff404@gmail.com>
2019-10-23 14:16:43 +0300

    Экран карточки вопроса - storybord progress

---
+++ CppQuiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CppQuiz
+//
+//  Created by Always Strive n Prosper on 20/10/2019.
+//  Copyright © 2019 komarov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
