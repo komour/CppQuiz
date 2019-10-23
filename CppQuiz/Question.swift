92d70ef13503aef0c9cc3fa76e9bc76ef447f0b1
Komarov <komaroff404@gmail.com>
2019-10-23 22:52:58 +0300

    keyboard hiding added

---
+++ CppQuiz/Question.swift
@@ -0,0 +1,14 @@
+//
+//  Question.swift
+//  CppQuiz
+//
+//  Created by Always Strive n Prosper on 23/10/2019.
+//  Copyright © 2019 komarov. All rights reserved.
+//
+
+import Foundation
+
+struct Question
+{
+    
+}
--- CppQuiz/ViewController.swift
+++ CppQuiz/ViewController.swift
@@ -12,9 +12,27 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupTextFields()
     }
-
+    
+    
+    @IBOutlet weak var textField2: UITextField!
+    @IBOutlet weak var textField1: UITextField!
+    func setupTextFields() {
+        let toolBar = UIToolbar(frame: CGRect(origin: .zero, size: .init(width: view.frame.size.width, height: 30)))
+        let flexSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let doneButton = UIBarButtonItem(title: "Done", style: .done
+            , target: self, action: #selector(doneButtonAction))
+        toolBar.setItems([flexSpace, doneButton], animated: false)
+        toolBar.sizeToFit()
+        
+        textField1.inputAccessoryView = toolBar
+        textField2.inputAccessoryView = toolBar
+    }
+    
+    @objc func doneButtonAction() {
+        self.view.endEditing(true)
+    }
 
 }
 
