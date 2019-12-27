196ff22fdb7a854f7cde526642019ae3bcd3a670
u-menya-lapki <komaroff404@gmail.com>
2019-12-27 05:49:38 +0300

    animations added

--- CppQuiz/ViewControllers/RightPopUpVC.swift
+++ CppQuiz/ViewControllers/RightPopUpVC.swift
@@ -12,12 +12,38 @@ class RightPopUpVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.showAnimate()
         self.view.backgroundColor = UIColor.black.withAlphaComponent(0.5)
         
     }
     @IBOutlet weak var resultLabel: UILabel!
     
     @IBAction func closePopUpButton() {
-        self.view.removeFromSuperview()
+        self.removeAnimate()
+//        self.view.removeFromSuperview()
     }
+    
+    func showAnimate()
+    {
+        self.view.transform = CGAffineTransform(scaleX: 1.3, y: 1.3)
+        self.view.alpha = 0.0;
+        UIView.animate(withDuration: 0.25, animations: {
+            self.view.alpha = 1.0
+            self.view.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
+        });
+    }
+    
+    func removeAnimate()
+    {
+        UIView.animate(withDuration: 0.25, animations: {
+            self.view.transform = CGAffineTransform(scaleX: 1.3, y: 1.3)
+            self.view.alpha = 0.0;
+            }, completion:{(finished : Bool)  in
+                if (finished)
+                {
+                    self.view.removeFromSuperview()
+                }
+        });
+    }
+
 }
